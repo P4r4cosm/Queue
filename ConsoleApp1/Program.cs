c79d6d66d499d07218bf48938462c17747a3ea17
P4r4cosm <113370500+P4r4cosm@users.noreply.github.com>
2023-05-11 18:55:40 +0300

    Queue

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,89 +2,93 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
+using System.Threading; 
 
 namespace ConsoleApp
 {
     class Program
     {
         
-        public class Queue<T> : IEnumerable<T> where T: IComparable<T>
+        public class Queue<T> : IEnumerable<T> where T: IComparable<T> 
+            //реализуем интрефейс IEnumerable для использования foreach,
+            //так же тип T должен реализовывать интерфейс IComparable
+            //Этот интерфейс определяет метод CompareTo(T other), который используется для сравнения объектов типа T.
+            //Он нужен для реализации сортировки
         {
             private T[] elements;
-            private int head;
-            private int tail;
+            private int head; //голова очереди
+            private int tail; //хвост очереди
 
-            public Queue()
+            public Queue() //O(1)
             {
-                elements = new T[0];
-                head = 0;
-                tail = -1;
+                elements = new T[0]; //O(1)
+                head = 0; //O(1)
+                tail = -1; //O(1)
             }
 
-            public void Enqueue(T element)
+            public void Push(T element) //добавляет элемент //O(1)
             {
-                Array.Resize(ref elements, elements.Length + 1);
-                tail++;
-                elements[tail] = element;
+                Array.Resize(ref elements, elements.Length + 1); //O(1)
+                tail++; //O(1)
+                elements[tail] = element; //O(1)
             }
 
-            public T Dequeue()
+            public T Pop() //Возвращает первый элемент очереди (добавленный раньше всех) и удаляет его //O(1)
             {
-                if (head > tail)
+                if (head > tail)//O(1)
                     throw new InvalidOperationException("Queue is empty");
 
-                T element = elements[head];
-                head++;
-                return element;
+                T element = elements[head]; //O(1)
+                head++; //O(1)
+                return element; //O(1)
             }
-            public T Peek()
+            public T Peek() //Возвращает первый элемент очереди (добавленный раньше всех) //O(1)
             {
-                if (head > tail)
+                if (head > tail) //O(1)
                     throw new InvalidOperationException("Queue is empty");
 
-                return elements[head];
+                return elements[head];//O(1)
             }
 
 
-            public int Count
+            public int Count //O(1)
             {
-                get { return tail - head + 1; }
+                get { return tail - head + 1; } //O(1)
             }
-            public void InsertionSort()
+            public void InsertionSort() //O(n^2)
             {
-                if (head == tail)
-                    return;
+                if (head == tail) //O(1)
+                    return; //O(1)
 
-                for (int i = head + 1; i <= tail; i++)
+                for (int i = head + 1; i <= tail; i++) //O(n^2)
                 {
-                    T current = elements[i];
-                    int j = i - 1;
+                    T current = elements[i]; //O(1)
+                    int j = i - 1; //O(1)
 
-                    while (j >= head && current.CompareTo(elements[j]) < 0)
+                    while (j >= head && current.CompareTo(elements[j]) < 0)  //O(n)
                     {
-                        elements[j + 1] = elements[j];
-                        j--;
+                        elements[j + 1] = elements[j]; //O(1)
+                        j--; //O(1)
                     }
 
-                    elements[j + 1] = current;
+                    elements[j + 1] = current;//O(1)
                 }
             }
 
 
-            public IEnumerator<T> GetEnumerator()
+            public IEnumerator<T> GetEnumerator() //O(n)
             {
-                int currentIndex = head;
-                for (int i = 0; i < Count; i++)
+                int currentIndex = head; //O(1)
+                for (int i = 0; i < Count; i++) //O(n)
                 {
-                    yield return elements[currentIndex];
-                    currentIndex = (currentIndex + 1) % elements.Length;
+                    yield return elements[currentIndex]; //O(1)
+                    currentIndex = (currentIndex + 1) % elements.Length; //O(1)
                 }
             }
 
-            IEnumerator IEnumerable.GetEnumerator()
+            IEnumerator IEnumerable.GetEnumerator() //O(n)
             {
-                return GetEnumerator();
+                return GetEnumerator(); //O(n)
             }
         }
         
@@ -92,29 +96,37 @@ namespace ConsoleApp
         static void Main()
         {
             Queue<int> queue = new Queue<int>();
-            queue.Enqueue(3);
-            queue.Enqueue(1);
-            queue.Enqueue(4);
-            queue.Enqueue(1);
-            queue.Enqueue(5);
-            queue.Enqueue(3);
-            queue.Enqueue(3);
-            queue.Enqueue(5);
-            queue.Enqueue(8);
-            queue.Enqueue(9);
-
-
-            foreach (int item in queue)
+            Random random = new Random();
+            for (int i=0; i < 20; i++) 
             {
-                Console.Write(item+" ");
-            }
-
-            queue.InsertionSort();
-            Console.WriteLine("После сортровки: ");
+                queue.Push(random.Next(0,100));
+            } //создаём очередь из случайных элементов
+            Console.WriteLine("Исходная очередь: ");
             foreach (int item in queue)
             {
                 Console.Write(item + " ");
-            }
+            } //выводим очередь
+
+
+            Console.WriteLine($"\n\nСработал метод Pop(): {queue.Pop()}"); //выписываем и удаляем самый первый элемент
+            Console.WriteLine("Очередь после метода Pop: ");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            } //выводим очередь
+            Console.WriteLine($"\n\nСработал метод Peek(): {queue.Peek()}"); //выписываем "новый" первый элемент
+            Console.WriteLine("Очередь после метода Peek: ");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            } //выводим очередь
+            
+            queue.InsertionSort(); //сортируем очередь
+            Console.WriteLine("\n\nПосле сортровки: ");
+            foreach (int item in queue)
+            {
+                Console.Write(item + " ");
+            } //выводим очередь
         }
     }
     
